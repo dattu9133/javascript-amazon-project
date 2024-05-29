7378b21a18d38aa735fee973df8a461b193bc6cf
dattu9133 <dattasaimoganti@gmail.com>
2024-05-29 20:16:25 +0530

    Moving all function related to cart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,34 @@
-export const cart = [];
+const cart = [];
+
+export function updateCartQuantity(productId, quantity) {
+  let matchingItem;
+
+  document.querySelector(`.added-to-cart-${productId}`).style.opacity = 1;
+
+  let timer = setTimeout(() => {
+    document.querySelector(`.added-to-cart-${productId}`).style.opacity = 0;
+  }, 2000);
+
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity || 1;
+  } else {
+    cart.push({
+      productId, quantity
+    });
+  }
+
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { updateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = '';
@@ -57,38 +57,6 @@ products.forEach((product) => {
   `;
 });
 
-function updateCartQuantity(productId, quantity) {
-  let matchingItem;
-
-  document.querySelector(`.added-to-cart-${productId}`).style.opacity = 1;
-
-  let timer = setTimeout(() => {
-    document.querySelector(`.added-to-cart-${productId}`).style.opacity = 0;
-  }, 2000);
-
-  cart.forEach((item) => {
-    if (productId === item.productId) {
-      matchingItem = item;
-    }
-  });
-
-  if (matchingItem) {
-    matchingItem.quantity += quantity || 1;
-  } else {
-    cart.push({
-      productId, quantity
-    });
-  }
-
-  let cartQuantity = 0;
-
-  cart.forEach((item) => {
-    cartQuantity += Number(item.quantity);
-  });
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
