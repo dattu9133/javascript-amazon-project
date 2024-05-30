b8767b98d9b8a163552fc795f005f5ff690fd2b6
dattu9133 <dattasaimoganti@gmail.com>
2024-05-30 21:56:10 +0530

    add checkout page functionality

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-const cart = [];
+export const cart = [{
+  "productId": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  "quantity": 3
+}, {
+  "productId": "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  "quantity": 2
+}
+];
 
 export function updateCartQuantity(productId, quantity) {
   let matchingItem;
@@ -28,6 +35,8 @@ export function updateCartQuantity(productId, quantity) {
   cart.forEach((item) => {
     cartQuantity += item.quantity;
   });
+
+  // console.log(cart)
 
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
